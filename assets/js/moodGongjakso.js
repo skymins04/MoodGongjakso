f45715d4df9e444313df0e82854b46f8a7baa118
skymins04 <skymin0417@gmail.com>
2021-01-12 08:06:57 +0900

    스크롤 다운 시 topmenu를 상단에 고정하는 js 이벤트 추가

--- assets/js/moodGongjakso.js
+++ assets/js/moodGongjakso.js
@@ -1,13 +1,16 @@
+var topMenu = document.getElementById('shop-header-menu');
 var topfixFlag = false;
 
 document.addEventListener('scroll', function(e) {
     var _y = window.scrollY;
-    if(_y >= 190 && !topfixFlag) {
+    var _boundary = 150;
+    if(_y >= _boundary && !topfixFlag) {
         //alert('fixed');
+        topMenu.setAttribute('class', 'topfix');
         topfixFlag = true;
     }
-    else if(_y < 190 && topfixFlag) {
-        //alert('unfixed');
+    else if(_y < _boundary && topfixFlag) {
+        topMenu.removeAttribute('class');
         topfixFlag = false;
     }
 });
